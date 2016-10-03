98aef2d6d2610dd1e3286a16f9a9ad8af408339d
Kimberly Nicholls <kimberly@gridium.com>
2016-10-02 22:52:02 -0600

    clean up extension

--- chrome-extension/bg.js
+++
@@ -1,1 +0,0 @@
-console.log('bg.js');
---
+++ chrome-extension/zip.sh
@@ -0,0 +1,1 @@
+zip leaderboard.zip *.css *.html *.js *.png manifest.json bower_components/keen-js/dist/keen.min.js bower_components/keen-js/dist/keen-tracker.min.js bower_components/jquery-1.11.1/dist/jquery.min.js
