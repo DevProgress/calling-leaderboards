fc573298ffa54fb24fa6316eec3feb86ee7f2b50
Kimberly Nicholls <kimberly@gridium.com>
2016-10-04 17:01:17 -0600

    consistent naming; use DOM to exchange data between popup & background

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -4,11 +4,18 @@ window.onload = function() {
     var style = doc.getElementById('keen-widgets');
     document.getElementById('keen-widgets').innerHTML = style.innerHTML || '';
     // copy content from background page to popup
-    ['totalCount', 'myCount', 'leaderboard'].forEach(function(id) {
+    ['totalCalls', 'myCalls', 'leaderboard'].forEach(function(id) {
         var el = doc.getElementById(id);
         document.getElementById(id).innerHTML = el.innerHTML || '';
     });
-    if ($('#totalCount').text().trim().startsWith('0')) {
+    var phonebankName = doc.getElementById('phonebankName').innerHTML;
+    if (phonebankName) {
+        $('#dashboardLink').attr('href', 'http://devprogress.us/calling-leaderboards/dashboard.html?name=phonebank.name&value='+encodeURIComponent(phonebank.name));
+    } else {
+        $('#dashboardLink').hide();
+    }
+    var total = $('#totalCount').text().trim();
+    if (total === '' || total.indexOf('0') === 0) {
         $('#widgets').hide();
         $('#noResults').show();
     } else {
